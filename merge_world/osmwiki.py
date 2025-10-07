d83e67fb9b75eefccf3a0375bd7c03744045adac
B.Ribon <b.ribon@dynartio.com>
2025-10-07 18:55:42 +0200

    Make path more robust

--- merge_world/osmwiki.py
+++ merge_world/osmwiki.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 import config
 
-source = "../osmwiki/openinframap_countries_info_brut.csv"
+source = Path(__file__).parent.parent / "osmwiki/openinframap_countries_info_brut.csv"
 destination = Path(config.DATA_FOLDER_PATH) / "00_WORLD" / "openinframap_countries_info_brut.csv"
 shutil.copy(source, destination)
 
-source = "../osmwiki/wikidata_countries_info_formatted.csv"
+source = Path(__file__).parent.parent / "osmwiki/wikidata_countries_info_formatted.csv"
 destination = Path(config.DATA_FOLDER_PATH) / "00_WORLD" / "wikidata_countries_info_formatted.csv"
 shutil.copy(source, destination)

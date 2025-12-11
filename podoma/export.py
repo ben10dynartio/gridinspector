dd40c45df9a6b19b2f434c201869a123f86c3c8b
B.Ribon <b.ribon@dynartio.com>
2025-12-11 16:12:15 +0100

    upgrade podoma and indicator map

--- podoma/export.py
+++ podoma/export.py
@@ -5,12 +5,21 @@ sys.path.append(str(Path(__file__).parent.parent / "common"))
 import configapps
 
 import os
-
+import argparse
 import psycopg2
 from psycopg2.extras import register_hstore
 
 import geopandas as gpd
 from shapely import wkb
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--objecttype", type=str, help="Object Type", default="lines")
+parser.add_argument("-c", "--country", type=str, help="Country OSM code", default="80500")
+
+args = parser.parse_args()
+countryosmcode = args.country
+objecttype = args.objecttype
+#80500
 
 # ---------------------------------------------
 # Connect to PostgreSQL/PostGIS database
@@ -45,47 +54,44 @@ query_dict["countryshapes"] = """
     FROM pdm_boundary WHERE admin_level = 2;
 """
 
-query_dict["lines"]  = """
+query_dict["lines"]  = f"""
 SELECT fc.osmid, fc.version, fc.tags, fl.label, fc.geom_len, fc.geom FROM pdm_features_lines_changes fc
 JOIN pdm_features_lines_boundary fb ON fb.osmid=fc.osmid AND fb.version=fc.version
 JOIN pdm_features_lines_labels fl ON fl.osmid=fc.osmid AND fl.version=fc.version
-WHERE fb.boundary=80500
+WHERE fb.boundary = {countryosmcode}
 AND fc.geom_len <> 0 
 AND fl.label='transmission'
 AND (CURRENT_TIMESTAMP BETWEEN fc.ts_start AND fc.ts_end OR (CURRENT_TIMESTAMP >= fc.ts_start AND fc.ts_end is null));
         """
 
-query_dict["points"]  = """
+query_dict["points"]  = f"""
 SELECT fm.memberid, fs.version, fs.tags, fs.geom FROM pdm_members_lines fm
 JOIN pdm_features_lines_boundary fb ON fm.osmid=fb.osmid AND fm.version=fb.version
 JOIN pdm_features_lines_changes fc ON fc.osmid=fm.osmid AND fc.version=fm.version
 LEFT JOIN pdm_features_supports_changes fs ON fm.memberid=fs.osmid AND fc.version=fs.version
-WHERE fb.boundary=80500
+WHERE fb.boundary = {countryosmcode}
 AND (CURRENT_TIMESTAMP BETWEEN fc.ts_start AND fc.ts_end OR (CURRENT_TIMESTAMP >= fc.ts_start AND fc.ts_end is null))
 AND (CURRENT_TIMESTAMP BETWEEN fs.ts_start AND fs.ts_end OR (CURRENT_TIMESTAMP >= fs.ts_start AND fs.ts_end is null));
 """
 
-query_dict["substations"]  = """
+query_dict["substations"]  = f"""
 SELECT fc.osmid, fc.version, fc.tags, fc.geom  FROM pdm_features_substations_changes fc
 JOIN pdm_features_substations_boundary fb ON fc.osmid=fb.osmid AND fc.version=fb.version
-WHERE fb.boundary=80500
+WHERE fb.boundary = {countryosmcode}
 AND (CURRENT_TIMESTAMP BETWEEN fc.ts_start AND fc.ts_end OR (CURRENT_TIMESTAMP >= fc.ts_start AND fc.ts_end is null));
 """
-
-
-commandkey = "lines"
 
 # ---------------------------------------------
 # Load data into a GeoDataFrame
 # ---------------------------------------------
-gdf = gpd.GeoDataFrame.from_postgis(query, conn, geom_col='geom')
+gdf = gpd.GeoDataFrame.from_postgis(query_dict[objecttype], conn, geom_col='geom')
 
 # ---------------------------------------------
 # Export to a shapefile
 # ---------------------------------------------
 output_path = configapps.OUTPUT_FOLDER_PATH / "pgsql"
 output_path.mkdir(exist_ok=True, parents=True)
-output_path = output_path / f"pdm_extract_{commandkey}.gpkg"
+output_path = output_path / f"pdm_extract_{objecttype}.gpkg"
 gdf.to_file(output_path)
 
 print("Shapefile created:", output_path)

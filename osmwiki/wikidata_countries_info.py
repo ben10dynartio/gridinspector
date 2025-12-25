f9c7bbe507866d49f9005b4c5a5c1c7ce5120446
B.Ribon <b.ribon@dynartio.com>
2025-12-25 09:52:43 +0100

    upgrade

--- osmwiki/wikidata_countries_info.py
+++ osmwiki/wikidata_countries_info.py
@@ -48,7 +48,7 @@ SPARQL_QUERY_COUNTRY_DEFINITION = """
   } UNION {
     # --- 2nd case : specific countries to add
     ?country wdt:P297 ?codeiso2 .
-    FILTER(?codeiso2 IN ('DK', 'PR', 'XK', 'GL', 'EH', 'AQ'))
+    FILTER(?codeiso2 IN ('DK', 'PR', 'XK', 'GL', 'EH', 'AQ', 'NC'))
   }"""
 
 def fetch_wikidata(query):
